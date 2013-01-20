1f18f150543c9fa3c4aef5ff6902d2e6e1f6c1ea
Pedro Jimenez <pedroj.1822@gmail.com>
2013-01-20 12:51:48 -0500

    cambie todas las url fijas dentro del html por constantes de PHP para que sea mas facil el cambio de algunos parametros

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('PROJECT_PATH', realpath(dirname(__file__)) . DS);
 define('easyPHP_PATH', PROJECT_PATH . 'easyPHP'. DS);
 define('CORE_PATH', easyPHP_PATH . 'core' . DS);
-define('BASE_URL', 'http://localhost/sell4me/');
+define('BASE_URL', 'http://localhost/shopline/');
 
 
 define('STATIC_DIR', BASE_URL . 'static' . DS);
@@ -13,6 +13,7 @@ define('easyPHP_TEMPLATES_DIR', easyPHP_PATH . 'default_templates' . DS);
 define('TEMPLATES_DIR', PROJECT_PATH . 'templates' . DS);
 
 define('PROJECT_NAME', "Shopline");
+define('GITHUB_URL', "https://github.com/house22/shopline");
 
 $DATABASE = array(
 	'engine' => 'mysql', //mysql, postgres, sqlite
